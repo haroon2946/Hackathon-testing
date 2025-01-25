df686a869b35856994bb8d57d9e56d91e1ac76d8
Haroon Afridi <afridi2542@gmail.com>
2025-01-25 12:16:24 +0500

    client

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@sanity/client";
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, // Ensure it's set
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "3ullc1fm", // Your Sanity project ID
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production", // Default to "production" if not set
   apiVersion: "2024-01-01", // Latest API version
-  useCdn: false, // False in development, true in production
+  useCdn: process.env.NODE_ENV === "production", // true for production, false for development
 });
